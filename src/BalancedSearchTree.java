334939928e74988a06788b3794786f52a7da6b04
unknown <svsquires@wisc.edu>
2018-02-26 15:55:37 -0600

    saved

--- src/BalancedSearchTree.java
+++ src/BalancedSearchTree.java
@@ -1,22 +1,69 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+// Semester:        CS400 Spring 2018
+// Project:         p2
+// Files:           BalancedSearchTree.java 
+//                  TestSearchTree.java 
+//                  SearchTreeADT.java 
+//                  DuplicateKeyException.java
+//
+// Author:          Stephen Squires III
+// Email:           svsquires@wisc.edu
+// Instructor:      Deb Deppeler
+// Due Date:        Monday, 2/26/2018
+//
+// Known Bugs:
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name:    Mary Xu
+// Partner Email:   yxu368@wisc.edu
+// Lecturer's Name: Deb Deppeler
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   _X_ Write-up states that pair programming is allowed for this assignment.
+//   _X_ We have both read and understand the course Pair Programming Policy.
+//   _X_ We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully 
+// acknowledge and credit those sources of help here.  Instructors and TAs do 
+// not need to be credited here, but tutors, friends, relatives, room mates 
+// strangers, etc do.  If you received no outside help from either type of 
+// source, then please explicitly indicate NONE.
+//
+// Persons:         (identify each person and describe their help in detail)
+// Online Sources:  (identify each URL and describe their assistance in detail)
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 
-
-// starter class for a BalancedSearchTree
-// you may implement AVL, Red-Black, 2-3 Tree, or 2-3-4 Tree
-// be sure to include in class header which tree you have implemented
+/**
+ * This is the balanced search tree class. It contains
+ * all the functionality required to add, and remove 
+ * items from a tree and maintain balance, as well as
+ * some calls that can be made on the tree to retrieve
+ * the size and contents.
+ * 
+ * @author Stephen Squires III
+ * @author Mary Xu
+ *
+ * @param <T> 
+ */
 public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeADT<T> {
 
-
-    // inner node class used to store key items and links to other nodes
-
+    // class fields
     protected Treenode<T> root;
     int height = 0;
 
+    
     public String inAscendingOrder() {
-
         return getAllItems(root);
-
     }
 
+    
+    /*
+     * (non-Javadoc)
+     * @see SearchTreeADT#isEmpty()
+     */
     public boolean isEmpty() {
 
         if (root == null){
@@ -27,41 +74,63 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
     }
 
+    
+    /*
+     * (non-Javadoc)
+     * @see SearchTreeADT#height()
+     */
     public int height() {
         return height;
     }
 
+    
+    /*
+     * (non-Javadoc)
+     * @see SearchTreeADT#lookup(java.lang.Comparable)
+     */
     public boolean lookup(T item) { //throws IllegalArgumentException{
 
         if (item == null) {
             return false;
         }
+    
         else {
             return lookupHelper(item, root);
         }
-        //	- if n's key equals the key, 
-        //	    - return true
-        //	- if (key < n's key) 
-        //	    - return lookup (left, key)
-        //	- return lookup (right, key)		
+        
     }
 
+    
+    /*
+     * (non-Javadoc)
+     * @see SearchTreeADT#insert(java.lang.Comparable)
+     */
     public void insert(T item) throws DuplicateKeyException { 
+    
         if(item == null) {
             throw new IllegalArgumentException();
         }
+        
         Treenode<T> newNode = new Treenode(item);
 
         if (root == null) {
+            height+=1;
             root = newNode;
             root.height = 1;
         }
 
         else {
+            height+=1;
             insertHelper(root, item);
         }
+        
     }
 
+    
+    /*
+     * (non-Javadoc)
+     * @see SearchTreeADT#delete(java.lang.Comparable)
+     */
     public void delete(T item) { //throws IllegalArgumentException{
         //TODO if item is null or not found in tree, return without error
         // else remove this item key from the tree and rebalance
@@ -77,8 +146,13 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
         // it will be at most 5% of the score for this program assignment
 
     }
-    private String getAllItems(Treenode<T> item) {
+   
     
+    /*
+     * 
+     */
+    private String getAllItems(Treenode<T> item) {
+       
         String a = "";
     
         if(isEmpty()) {
@@ -99,18 +173,25 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     
             return a;
         }
-    
+        
     }
 
+    
+    /*
+     * 
+     */
     private Treenode<T> deleteHelper(Treenode<T> currNode, T key) {
 
         if(currNode == null) {
             return null;
         }
+        
         int comesAfter = currNode.key.compareTo(key);
+        
         if(comesAfter < 0) {
             currNode.right = deleteHelper(currNode.right, key);
         }
+        
         if(comesAfter > 0) {
             currNode.left = deleteHelper(currNode.left, key);
         }
@@ -120,15 +201,23 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
             if(currNode.left == null) {
                 return currNode.right;
             }
+            
             else if(currNode.right == null) {
                 return currNode.left;
             }
+            
         }
+        
         currNode.key = findMinValue(root.right).key;
         currNode.right = deleteHelper(currNode.right, currNode.key);
         return currNode;
+        
     }
 
+    
+    /*
+     * 
+     */
     private void insertHelper(Treenode<T> currNode, T key) throws DuplicateKeyException {
     
             if(lookup(key)) {
@@ -190,6 +279,10 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     
         }
 
+    
+    /*
+     * 
+     */
     private boolean lookupHelper(T item, Treenode<T> node) {
 
         boolean lookup = false;
@@ -204,22 +297,24 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
         }
 
         else {
+            
             if(item.compareTo(node.key)<0) {
                 lookup = lookupHelper(item, node.left);
             }
+            
             if(item.compareTo(node.key)>0) {
                 lookup = lookupHelper(item, node.right);
             }
+            
             return lookup;
         }
+        
     }
 
     
-
-    // HINT: define this helper method that can find the smallest key 
-    // in a sub-tree with "node" as its root
-    // PRE-CONDITION: node is not null
-
+    /*
+     * 
+     */
     private void rightRotate(Treenode <T> node, T key) {
         Treenode<T> newParent = node.left;
         node.left = newParent.right;
@@ -228,7 +323,11 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
         node.height = max(getHeightOfNode(newParent.left), getHeightOfNode(newParent.right)) +1;
         height = node.height;
     }
-
+    
+    
+    /*
+     * 
+     */
     private void leftRotate(Treenode <T> node, T key) {
         Treenode<T> newParent = node.right;
         node.right = newParent.left;
@@ -238,48 +337,66 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
         height = node.height;
     }
 
-    // HINT: define this helper method that can find the smallest key 
-    // in a sub-tree with "node" as its root
-    // PRE-CONDITION: node is not null
     
+    /*
+     * 
+     */
     private int getHeightOfNode(Treenode<T> node) {
+        
         if(node == null) {
             return 0;
         }
+        
         return node.height;
+        
     }
 
+    
+    /*
+     * 
+     */
     private int max(int height1, int height2) {
+       
         if(height1 > height2) {
             return height1;
         }
+        
         else if (height1 < height2) {
             return height2;
         }
+        
         else {
             return height1;
         }
+        
     }
+    
+    /*
+     * 
+     */
     private Treenode<T> findMinValue(Treenode<T> currNode) {
+        
         Treenode <T> minRightValue = null;
+        
         while(currNode != null) {
             minRightValue = findMinValue(currNode.left);
         }
+        
         return minRightValue;
+        
     }
 
-    
 
-    // HINT: define this helper method that can find the smallest key 
-    // in a sub-tree with "node" as its root
-    // PRE-CONDITION: node is not null
     
-    private T leftMost(Treenode<T> node) {
-        // TODO return the key value of the left most node in this subtree
-        // or return node's key if node does not have a left child
-        return node.key;
-    }
-
+    
+    
+    /**
+     * 
+     * @author squir
+     *
+     * @param <K>
+     */
+    
     protected class Treenode<K extends Comparable<K>> {
 
         K key;
