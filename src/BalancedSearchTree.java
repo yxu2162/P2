b3783d40689c9d596a521e0ddd046479d00c1dda
Mary <yxu368@wisc.edu>
2018-02-26 20:56:56 -0600

    messed something up

--- src/BalancedSearchTree.java
+++ src/BalancedSearchTree.java
@@ -55,24 +55,14 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     private int height = 0;
 
 
-    /**
-     * Returns a string representation of the contents 
-     * of the search tree sorted in ascending order. Each
-     * item has a space between it and the next.
-     * 
-     * @return string of items with spaces between each
-     *          in ascending order
-     */
     public String inAscendingOrder() {
         return getAllItems(root);
     }
 
 
     /**
-     * Checks whether the search tree is or is not empty.
-     * 
-     * @return boolean value of true for empty and false 
-     *          for not empty
+     * (non-Javadoc)
+     * @see SearchTreeADT#isEmpty()
      */
     public boolean isEmpty() {
 
@@ -86,33 +76,26 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
 
     /**
-     * Returns the height of the longest path of the tree
-     * 
-     * @return int value of height of tree
+     * (non-Javadoc)
+     * @see SearchTreeADT#height()
      */
     public int height() {
-
         if(isEmpty()) {
             return 0;
         }
-
         else if(root.left == null && root.right == null) {
             return 1;
         }
+//        System.out.println(root.key);
+//        System.out.println(root.left.key);
+//        System.out.println(root.right.key);
         return max(getHeightOfNode(root.left), getHeightOfNode(root.right)) +1;
-
     }
 
 
     /**
-     * Searches the tree for an item and tells you whether
-     * or not it's there.
-     * 
-     * @param item of generic comparable type T that you
-     *          are searching the tree for
-     *          
-     * @return boolean true if the item is found, false
-     *          if it is not
+     * (non-Javadoc)
+     * @see SearchTreeADT#lookup(java.lang.Comparable)
      */
     public boolean lookup(T item) { //throws IllegalArgumentException{
 
@@ -128,10 +111,8 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
 
     /**
-     * Inserts an item into the tree in its proper location
-     * to maintain the BST order and shape properties.
-     * 
-     * @param item of generic type T to be inserted into the tree
+     * (non-Javadoc)
+     * @see SearchTreeADT#insert(java.lang.Comparable)
      */
     public void insert(T item) throws DuplicateKeyException { 
 
@@ -153,32 +134,27 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
 
     /**
-     * Deletes an item from the tree and replaces it with
-     * its successor.
-     * 
-     * @param item of generic type T to be deleted from the BST
+     * (non-Javadoc)
+     * @see SearchTreeADT#delete(java.lang.Comparable)
      */
-    public void delete(T item) { 
-
+    public void delete(T item) { //throws IllegalArgumentException{
+        //TODO if item is null or not found in tree, return without error
+        // else remove this item key from the tree and rebalance
         if(item == null || !lookup(item)) {
             return;
         }
 
         else {
-            deleteHelper(root, item);
+            deleteHelper(this.root, item);
         }
+
+        // NOTE: if you are unable to get delete implemented
+        // it will be at most 5% of the score for this program assignment
 
     }
 
 
     /**
-     * Returns a string of all items in the subtree rooted
-     * at item in ascending order.
-     * 
-     * @param item root of subtree of items to return in string
-     * 
-     * @return string of items in the tree separated by spaces
-     *          in ascending order
      * 
      */
     private String getAllItems(Treenode<T> item) {
@@ -197,6 +173,7 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
             if(item.key != null) {
                 a += item.key + " ";
+
             }
 
             if(item.right != null) { 
@@ -205,21 +182,9 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
             return a;
         }
-
     }
 
-
-    /** 
-     * Steps through the nodes in the tree to find the one
-     * that matches the key. Once found, it finds the leaf 
-     * that will take its place and updates the parent and
-     * children references to keep the tree working properly.
-     * 
-     * @param currNode of type Treenode<T> starting node in 
-     *          the search for the node that will be deleted
-     *          
-     * @param key of generic type T the key of the item being
-     * searched for
+    /*
      * 
      */
     private void deleteHelper(Treenode<T> currNode, T key) {
@@ -329,34 +294,24 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
                 }
 
             }
-
         }
-
     }
 
 
-    /**
-     * Searches the tree for the child node and returns its parent.
-     * 
-     * @param currNode of type Treenode<T> starting node of the subtree 
-     *          where it will search for the child
-     *          
-     * @param child of type Treenode<T> node that is being searched
-     *          for so that its parent can be returned
-     *          
-     * @return Treenode<T> parent of the aforepassed child node
-     */
+
+
+
+
+
     private Treenode<T> getParent(Treenode<T> currNode, Treenode<T> child) {
 
         int comesAfter = currNode.key.compareTo(child.key);
 
         if (currNode.left.key != null) {
             int compareLeft = currNode.left.key.compareTo(child.key);
-
             if (compareLeft == 0) {
                 return currNode;
             }
-
         }
 
         if (currNode.right.key != null) {
@@ -381,17 +336,8 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     }
 
 
-    /**
-     * Helper for the insert method. Searches the tree top to 
-     * bottom for the leaf node where it fits to maintain the 
-     * BST order and shape properies. Once inserted, the tree
-     * should balance itself with left-right roatons.
+    /*
      * 
-     * @param currNode of type Treenode<T> the root node of the
-     *          current subtree being traversed
-     * 
-     * @param key of generic type T the key of the node being
-     *          inserted
      */
     private void insertHelper(Treenode<T> currNode, T key) throws DuplicateKeyException {
 
@@ -406,11 +352,9 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
             if (currNode.left != null) { 
                 insertHelper(currNode.left, key); 
             }
-
             else {
                 currNode.left = new Treenode<T>(key);
             }
-
         }
 
         if (comesAfter < 0) {
@@ -424,81 +368,33 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
             }
 
         }
-        checkBalance(currNode, key);
-    }
-    private void checkBalance(Treenode<T> currNode, T key) {
-
+        
         currNode.height = max(getHeightOfNode(currNode.left), getHeightOfNode(currNode.right)) +1;
         int balanceFactor = getHeightOfNode(currNode.left)-getHeightOfNode(currNode.right);
-
-        if(currNode.right != null) {
-
-            if(currNode.right != null && balanceFactor == -2 && 
-                    key.compareTo(currNode.right.key) > 0) { //left rotate
-                System.out.println("Left Rotate");
-                leftRotate(currNode);
-            }
-
-            if(balanceFactor == -2) {
-                leftRotate(currNode);
-                balanceFactor = getHeightOfNode(currNode.left)-getHeightOfNode(currNode.right);
-                if(balanceFactor == 2) {
-                    rightRotate(currNode);
-                }
-
-            }
+        if(currNode.key.equals("C")) {
+            System.out.println(currNode.height);
+        }
+        if(balanceFactor == -2) {
+            leftRotate(currNode);
+            balanceFactor = getHeightOfNode(currNode.left)-getHeightOfNode(currNode.right);
             if(balanceFactor == 2) {
                 rightRotate(currNode);
-                balanceFactor = getHeightOfNode(currNode.left)-getHeightOfNode(currNode.right);
-                if(balanceFactor == -2) {
-                    rightRotate(currNode);
-                }
-                if(currNode.right != null && balanceFactor == -2 && 
-                        key.compareTo(currNode.right.key) > 0) { //right left rotate
-                    System.out.println("Right Rotate");
-                    rightRotate(currNode);
-                    leftRotate(currNode);
-                }
-
             }
-
-            if (currNode.left != null) {
-
-                if(currNode.left != null && balanceFactor == 2 && 
-                        key.compareTo(currNode.left.key) < 0) { //right rotate
-                    System.out.println("Right rotate");
-                    rightRotate(currNode);
-                }
-
-                if(currNode.left != null && balanceFactor == 2 && 
-                        key.compareTo(currNode.left.key) < 0) { //left right rotate
-                    System.out.println("Right rotate");
-                    leftRotate(currNode);
-                    rightRotate(currNode);
-
-                }
+            
+        }
+        if(balanceFactor == 2) {
+            rightRotate(currNode);
+            balanceFactor = getHeightOfNode(currNode.left)-getHeightOfNode(currNode.right);
+            if(balanceFactor == -2) {
+                rightRotate(currNode);
             }
         }
-
+    }
+    private void checkBalance(Treenode<T> currNode, T key) {
+        
     }
     /*
-=======
-
-
-    /**
-     * Helper for the lookup method. Traverses the tree
-     * to check whether or not an item exists.
->>>>>>> d67bf72b03cbfb8d963dfab934b82857419130be
      * 
-     * @param node of type Treenode<T> being examined and
-     *          its key being compared against the input
-     *          key to check for a match
-     *          
-     * @param item of generic type T being searched for in
-     *          the tree
-     *          
-     * @return boolean true if the item is found false if
-     *          it is not
      */
     private boolean lookupHelper(T item, Treenode<T> node) {
 
@@ -529,10 +425,8 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     }
 
 
-    /**
-     * Rotates 3 nodes right about the input node
+    /*
      * 
-     * @param node of type Treenode<T> to rotate about
      */
     private void rightRotate(Treenode <T> node) {
         Treenode<T> newParent = node.left;
@@ -546,12 +440,10 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     }
 
 
-    /**
-     * Rotates 3 nodes left about the input node
+    /*
      * 
-     * @param node of type Treenode<T> to rotate about
      */
-    private void leftRotate(Treenode<T> node) {
+    private void leftRotate(Treenode <T> node) {
         Treenode<T> newParent = node.right;
         node.right = newParent.left;
         newParent.left = node;
@@ -563,20 +455,14 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     }
 
 
-    /**
-     * Gets the height of the specified node
+    /*
      * 
-     * @param node of type Treenode<T> the height of which
-     *          is returned
-     *          
-     * @return int value of the height of the specified node
      */
     private int getHeightOfNode(Treenode<T> node) {
 
         if(node != null) {
             return node.height;
         }
-
         else {
             return 0;
         }
@@ -584,15 +470,8 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     }
 
 
-    /** 
-     * Compares the height of 2 nodes and returns
-     * the larger height
+    /*
      * 
-     * @param height1 of type int that is being compared
-     * 
-     * @param height2 of type int that is being compared
-     * 
-     * @return int higher of the 2 heights
      */
     private int max(int height1, int height2) {
 
@@ -610,14 +489,8 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 
     }
 
-
-    /**
-     * Finds the minimum value of the specified subtree
+    /*
      * 
-     * @param currNode of type Treenode<T> the root of the 
-     *          subtree being searched for its smallest node
-     *          
-     * @return Treenode<T> the smallest node of the subtree         
      */
     private Treenode<T> findMinValue(Treenode<T> currNode) {
 
@@ -631,14 +504,16 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     }
 
 
+
+
+
+
+
     /**
-     * This class contains the contents of the treenodes in the
-     * BST. It has pointers to the left and right children as well
-     * as a key to identify it. 
      * 
-     * @author Stephen Squires III
-     * @author Mary Xu
-     * @param <K> comparabe generic data type
+     * @author squir
+     *
+     * @param <K>
      */
 
     protected class Treenode<K extends Comparable<K>> {
@@ -646,29 +521,12 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
         K key;
         Treenode<K> left;
         Treenode<K> right;
-        int height;             // height of the node
+        int height;
 
-        /**
-         * Constructor for the treenode item
-         * 
-         * @param item of generic type K that contains
-         *          the contents of the node
-         */
         public Treenode(K item) {
             this(item,null,null);
         }
 
-        /**
-         * Constructor for the treenode that sets its key,
-         * as well as left and right children
-         * 
-         * @param item of generic type K contains the contents
-         *          of the node
-         *          
-         * @param left of type Treenode<K> that references the left child
-         * 
-         * @param right of type Treenode<K> what references the right child
-         */
         public Treenode(K item, Treenode<K> left, Treenode<K> right) {
             key = item;
             this.left = left;
